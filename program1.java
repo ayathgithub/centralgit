fd3151cc8f83522e8fbc76da702b9adeac79d5be
root <root@ip-172-31-2-168.ap-south-1.compute.internal>
2022-03-23 07:01:47 +0000

    java

---
+++ program1.java
@@ -0,0 +1,22 @@
+class  program1
+{
+	public static void main(String[] args)
+	{  
+		int x =10;
+		double y =32.32;
+        char c ='a';
+		long l =43543;
+		float f =76.54f;
+		boolean b =true;
+		String s = "Jspider";
+
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("c="+c);
+		System.out.println("l="+l);
+		System.out.println("f="+f);
+		System.out.println("b="+b);
+		System.out.println("s="+s);
+	}
+}
+
